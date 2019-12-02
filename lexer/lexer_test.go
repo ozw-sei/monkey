4f9d6913ba78b9cee58797910fd69cf332cf7020
ozawaseijiro <seijiro4751@gmail.com>
2019-12-02 11:22:12 +0900

    fail test

---
+++ lexer/lexer_test.go
@@ -0,0 +1,39 @@
+package lexer
+
+import (
+	"github.com/ozw-sei/monkey/token"
+	"testing"
+)
+
+func textNextToken(t *testing.T) {
+	input := `=+(){},;`
+
+	test := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+		
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong.expected=%q, got=%q", i, tt.expectedType, tok.Literal)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tt.Literal)
+		}
+	}
+}
