73ae89aa3893119816166d9cd13c9059afa16620
ozawaseijiro <seijiro4751@gmail.com>
2019-12-03 22:09:15 +0900

    fix bug

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -5,23 +5,69 @@ import (
 	"testing"
 )
 
-func textNextToken(t *testing.T) {
-	input := `=+(){},;`
+func TestNextToken(t *testing.T) {
+	input := `let five = 7;
+let ten = 10;
+let add = fn(x, y) {
+x + y;
+}
+
+let result = add(five, ten);
+`
 
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
+		// let five = 5;
+		{token.LET, "let"},
+		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		// let five = 10;
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+
+		// let add = fn(x, y){
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
+
+		// x + y;
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
 		{token.SEMICOLON, ";"},
+
+		// };
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+
+		// let result = add(five, ten)
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+
 		{token.EOF, ""},
-		
 	}
 
 	l := New(input)
@@ -37,3 +83,4 @@ func textNextToken(t *testing.T) {
 		}
 	}
 }
+
--- token/token.go
+++ token/token.go
@@ -3,25 +3,24 @@ package token
 type TokenType string
 
 type Token struct {
-	Type TokenType
+	Type    TokenType
 	Literal string
 }
 
 const (
-	
 	ILLEGAL = "ILLEGAL"
-	EOF = "EOF"
-	
+	EOF     = "EOF"
+
 	// 識別子
 	IDENT = "IDENT"
-	INT = "INT"
-	
+	INT   = "INT"
+
 	// Token
 	ASSIGN = "="
-	PLUS = "+"
-	
+	PLUS   = "+"
+
 	// デリミタ
-	COMMA = ","
+	COMMA     = ","
 	SEMICOLON = ";"
 
 	LPAREN = "("
@@ -29,7 +28,19 @@ const (
 	LBRACE = "{"
 	RBRACE = "}"
 
-
 	FUNCTION = "FUNCTION"
-	LET = "LET"
+	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
